ff751743944a102ec3d63f1a71b79fe2fe40732d
lencx <cxin1314@gmail.com>
2022-07-05 07:23:24 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
+
+const md = MarkdownIt({
+  highlight: function (str: string, lang: string) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(str, { language: lang }).value;
+      } catch (__) {}
+    }
+    return ''; // use external default escaping
+  }
+});
